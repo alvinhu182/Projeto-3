419a19e7e1310f8ad1fd45899d315831be32fe02
alvinhu182 <alvinhu@hotmail.com>
2022-05-24 20:46:17 -0300

    instalando fonte latos

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
+import { GlobalStyle } from './assets/css/global';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,6 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
    <BrowserRouter>
+   <GlobalStyle/>
     <App />
     <ToastContainer/>
     </BrowserRouter>
