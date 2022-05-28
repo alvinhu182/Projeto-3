9237e21e7d9ad0b145a847d4219d3a4f42d68a0b
alvinhu182 <alvinhu@hotmail.com>
2022-05-28 00:19:57 -0300

    iniciando o cadastro

--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -1,11 +1,13 @@
 import {Route, Routes as RDRoutes} from "react-router-dom"
 import { HomeView } from "./views/home"
 import { NotFoundView } from "./views/NotFound"
+import { RegisterView } from "./views/Register"
 export function Routes () {
     return (
       <RDRoutes>
           <Route path='/' element={<HomeView />} />
           <Route path='*' element={<NotFoundView/>} />
+          <Route path='/cadastro' element={<RegisterView/>} />
 
       </RDRoutes>  
     )
---
+++ front-end/src/views/Register/index.tsx
@@ -0,0 +1,22 @@
+import { Container } from "react-bootstrap";
+import { FormField } from "../../components/FormField";
+import { Layout } from "../../components/Layout";
+import { PageTitle } from "../../components/PageTitle";
+
+export function RegisterView () {
+    return (
+        <Layout>
+            <Container>
+                <PageTitle> Nova conta</PageTitle>
+                <FormField
+                controlId="userName"
+                label="Nome"
+                placeholder="Digite o seu nome"
+                error="Preencha o seu nome."
+                isInvalid
+                
+                />
+            </Container>
+        </Layout>
+    )
+}
